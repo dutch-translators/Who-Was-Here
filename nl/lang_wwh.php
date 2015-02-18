f328db3d4ac329cbbb2ba6e4efd5632a6d14b4a4
David <neurofibromatose@live.nl>
2015-02-18 11:34:09 +0100

    eerste vertaling

--- nl/lang_wwh.php
+++ nl/lang_wwh.php
@@ -4,6 +4,7 @@
 * @package - NV "who was here?"
 * @copyright (c) nickvergessen - http://www.flying-bits.org/
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 *
 */
 if (!defined('IN_PHPBB'))
@@ -17,24 +18,24 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 // for the normal sites
-	'WHO_WAS_HERE'					=> 'Who was here?',
-	'WHO_WAS_HERE_LATEST1'			=> 'last at',
+	'WHO_WAS_HERE'					=> 'Wie was hier?',
+	'WHO_WAS_HERE_LATEST1'			=> 'laatst op',
 	'WHO_WAS_HERE_LATEST2'			=> '',//used for parts like o'clock in the timedisplay (last at vw:xy "o'clock")
 
 	'WHO_WAS_HERE_TOTAL'			=> array(
-		0		=> 'In total there were <strong>0</strong> users online :: ',
-		1		=> 'In total there was <strong>%d</strong> user online :: ',
-		2		=> 'In total there were <strong>%d</strong> users online :: ',
+		0		=> 'In totaal waren er <strong>0</strong> gebruikers online :: ',
+		1		=> 'In totaal was er <strong>%d</strong> gebruiker online :: ',
+		2		=> 'In totaal waren er <strong>%d</strong> gebruikers online :: ',
 	),
 	'WHO_WAS_HERE_REG_USERS'		=> array(
-		0		=> '0 registered',
-		1		=> '%d registered',
-		2		=> '%d registered',
+		0		=> '0 geregistreerde gebruikers',
+		1		=> '%d geregistreerde gebruiker',
+		2		=> '%d geregistreerde gebruikers',
 	),
 	'WHO_WAS_HERE_HIDDEN'			=> array(
-		0		=> '0 hidden',
-		1		=> '%d hidden',
-		2		=> '%d hidden',
+		0		=> '0 verborgen',
+		1		=> '%d verborgen',
+		2		=> '%d verborgen',
 	),
 	'WHO_WAS_HERE_BOTS'				=> array(
 		0		=> '0 bots',
@@ -42,31 +43,31 @@ $lang = array_merge($lang, array(
 		2		=> '%d bots',
 	),
 	'WHO_WAS_HERE_GUESTS'			=> array(
-		0		=> '0 guests',
-		1		=> '%d guest',
-		2		=> '%d guests',
+		0		=> '0 gasten',
+		1		=> '%d gast',
+		2		=> '%d gasten',
 	),
 
-	'WHO_WAS_HERE_WORD'				=> ' and',
-	'WHO_WAS_HERE_EXP'				=> 'based on users active today',
-	'WHO_WAS_HERE_EXP_TIME'			=> 'based on users active over the past ',
+	'WHO_WAS_HERE_WORD'				=> ' en',
+	'WHO_WAS_HERE_EXP'				=> 'gebaseerd op de gebruikers die vandaar waren',
+	'WHO_WAS_HERE_EXP_TIME'			=> 'gebaseerd op gebruikers die actief waren in de laatste ',
 	'WWH_HOURS'						=> array(
 		0		=> '',
-		1		=> '%%s %1$s hour',
-		2		=> '%%s %1$s hours',
+		1		=> '%%s %1$s uur',
+		2		=> '%%s %1$s uren',
 	),
 	'WWH_MINUTES'					=> array(
 		0		=> '',
-		1		=> '%%s %1$s minute',
-		2		=> '%%s %1$s minutes',
+		1		=> '%%s %1$s minuut',
+		2		=> '%%s %1$s minuten',
 	),
 	'WWH_SECONDS'					=> array(
 		0		=> '',
-		1		=> '%%s %1$s second',
-		2		=> '%%s %1$s seconds',
+		1		=> '%%s %1$s seconde',
+		2		=> '%%s %1$s seconden',
 	),
-	'WHO_WAS_HERE_RECORD'			=> 'Most users ever online was <strong>%1$s</strong> on %2$s',
-	'WHO_WAS_HERE_RECORD_TIME'		=> 'Most users ever online was <strong>%1$s</strong> between %2$s and %3$s',
+	'WHO_WAS_HERE_RECORD'			=> 'De meeste gebruikers tegelijkertijd online ooit was <strong>%1$s</strong> op %2$s',
+	'WHO_WAS_HERE_RECORD_TIME'		=> 'De meeste gebruikers tegelijkertijd online ooit was <strong>%1$s</strong> tussen %2$s en %3$s',
 ));
 
 ?>
